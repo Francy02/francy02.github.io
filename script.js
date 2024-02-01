792f24c74a0445219d542c4ccaa652949a29681f
Francy02 <franciliaci@gmail.com>
2024-02-01 12:02:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "SI GODEE! Ti amo tanto Bimba💚";
+    yesMessage.textContent = "SI GODEE! Ti amo tanto Bimba💚 (ps. presto arriverà qualcosa a casa👀)";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
